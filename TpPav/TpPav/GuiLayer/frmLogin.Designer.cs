ec1bcdf186656ae00ddca560ee11fdd6b049ddb3
ElianaCassini <ecassini@baufest.com>
2021-11-09 12:39:02 -0300

    ultimos cambios

--- TpPav/TpPav/GuiLayer/frmLogin.Designer.cs
+++ TpPav/TpPav/GuiLayer/frmLogin.Designer.cs
@@ -115,6 +115,7 @@ namespace TpPav.GuiLayer
             // 
             // frmLogin
             // 
+            this.AcceptButton = this.btnIngresar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
